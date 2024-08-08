dde5d2bb892f8b19d0bc71220de31f4711a04f76
Devanand <dk461298@gmail.com>
2024-08-08 17:07:51 +0530

    third

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     images: {
         domains: ['encrypted-tbn2.gstatic.com','content.tupaki.com','i.pinimg.com','www.radioandmusic.com','images.unsplash.com' ,'rescloudinary.com',]
     }
